71ad6ab127e76341f6febd13f4385ca003fdfbbe
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-25 17:42:56 -0500

    codigo arreglado

--- main.tf
+++ main.tf
@@ -14,14 +14,3 @@ resource "aws_s3_bucket" "report_output_bucket" {
   force_destroy = true
 }
 
-# 🔔 Notificación de eventos al subir objetos al bucket de entrada
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.csv_input_bucket.id
-
-  lambda_function {
-    lambda_function_arn = var.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.allow_s3_invoke]
-}
