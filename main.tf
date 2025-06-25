6e1840d32812d81b867b1707c331b0511bf3d481
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-25 14:44:17 -0500

    mejoras

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ resource "aws_s3_bucket" "report_output_bucket" {
 }
 
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.csv_input_bucket.id
+  resource "aws_s3_bucket_notification" "bucket_notification" {
+    bucket = aws_s3_bucket.csv_input_bucket.id
 
-  lambda_function {
-    lambda_function_arn = var.lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
+    lambda_function {
+      lambda_function_arn = var.lambda_function_arn
+      events              = ["s3:ObjectCreated:*"]
+    }
+
+    depends_on = [aws_lambda_permission.allow_s3_invoke]
   }
-
-  depends_on = [aws_lambda_permission.allow_s3_invoke]
-}
