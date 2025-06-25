7d07ad0667b4a54de61dbb9f539e6c61c417c3a5
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-25 08:06:45 -0500

    codigo arreglado

--- main.tf
+++ main.tf
@@ -5,19 +5,9 @@ provider "aws" {
 resource "aws_s3_bucket" "csv_input_bucket" {
   bucket        = "proyecto-csv-entrada-2025"
   force_destroy = true
-
-  tags = {
-    Name        = "proyecto-csv-entrada-2025"
-    Environment = var.env
-  }
 }
 
 resource "aws_s3_bucket" "report_output_bucket" {
   bucket        = "proyecto-reportes-procesados"
   force_destroy = true
-
-  tags = {
-    Name        = "proyecto-reportes-procesados"
-    Environment = var.env
-  }
 }
