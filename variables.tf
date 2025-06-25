bf21f53e1122a725b7242d290ac94beeeb61f4bc
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-25 17:40:02 -0500

    codigo arreglado

--- variables.tf
+++ variables.tf
@@ -13,13 +13,3 @@ variable "output_bucket_name" {
   description = "Nombre del bucket de salida para reportes"
   type        = string
 }
-
-variable "lambda_function_name" {
-  description = "Nombre de la función Lambda"
-  type        = string
-}
-
-variable "lambda_function_arn" {
-  description = "ARN de la función Lambda"
-  type        = string
-}
