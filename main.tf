c819c8f26d1b86b5979c08fe0b81f0f339765017
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-27 10:00:34 -0500

    cambios de variables

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
 
 # 🪣 Bucket de entrada CSV
 resource "aws_s3_bucket" "csv_input_bucket" {
-  bucket        = "proyecto-csv-entrada-2025"
+  bucket        = "proyecto-csv-entrada"
   force_destroy = true
 }
 
 # 🪣 Bucket de salida de reportes
 resource "aws_s3_bucket" "report_output_bucket" {
-  bucket        = "proyecto-reportes-procesados"
+  bucket        = "proyecto-csv-salida"
   force_destroy = true
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 aws_region         = "us-east-1"
 
-input_bucket_name  = "proyecto-csv-entrada-2025"
-output_bucket_name = "proyecto-reportes-procesados"
+  input_bucket_name   = "proyecto-csv-entrada"
+  output_bucket_name  = "proyecto-csv-salida"
