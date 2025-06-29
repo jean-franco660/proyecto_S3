49459d2665704bd64f87be3e42a3ba316efa5a66
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-29 07:20:00 -0500

    creacion de los buckets S3

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "input_bucket_name" {
   description = "Nombre del bucket de entrada"
-  value       = aws_s3_bucket.input_bucket_name.bucket
+  value       = aws_s3_bucket.csv_input_bucket.bucket
 }
 
 output "output_bucket_name" {
   description = "Nombre del bucket de salida"
-  value       = aws_s3_bucket.output_bucket_name.bucket
+  value       = aws_s3_bucket.csv_output_bucket.bucket
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
 input_bucket_name  = "archivo-csv-entrada"
 output_bucket_name = "archivo-csv-salida"
-lambda_function_arn = "arn:aws:lambda:us-east-1:123456789012:function:process_csv_lambda"  # Reemplazar con valor real
+lambda_function_arn = "arn:aws:lambda:us-east-1:740857578543:function:lambda_reportes_csv"
